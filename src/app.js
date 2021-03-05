46475531a328f915e66bda2351de9e2c25dac471
Renan Alves <coelhoeliteyt@gmail.com>
2021-03-05 19:55:31 -0300

    Tratamento de erros | novas dependencias

--- src/app.js
+++ src/app.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan')
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
 const rotaUsuarios = require('./routes/usuarios');
 
+app.use(morgan('dev'));
+
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
 app.use('/usuarios', rotaUsuarios);
 
+// Quando não encontra rota, entra aqui;
 app.use((req, res, next) => {
-    res.status(200).send({
-        title: 'Node API',
-        msg: 'Ok, está funcionado'
+    const erro = new Error('não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
     });
 });
 
